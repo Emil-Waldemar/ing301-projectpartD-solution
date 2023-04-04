ac9eccca4435d2251fd0ff97981997edf3e85721
Lars Michael Kristensen <lmkr@users.noreply.github.com>
2023-04-04 12:19:50 +0200

    temperature widgets

--- dashboard.py
+++ dashboard.py
@@ -8,16 +8,14 @@ root = tk.Tk()
 
 # configure the root window
 
-root.geometry('300x200')
-root.resizable(False, False)
+root.geometry('300x300')
+#root.resizable(True, True)
 root.title('ING301 SmartHouse Dashboard')
 
-# label frame
+# HeatOven control
 
-
-
-lf = ttk.LabelFrame(root, text='Heat Oven [X]')
-lf.grid(column=0, row=0, padx=20, pady=20)
+hc_lf = ttk.LabelFrame(root, text='Heat Oven [X]')
+hc_lf.grid(column=0, row=0, padx=20, pady=20, sticky=tk.W)
 
 heater_state_var = tk.StringVar()
 heater_state_var.set('Off')
@@ -25,17 +23,41 @@ heater_state_var.set('Off')
 heater_states = ('On', 'Off')
 
 
-def sel():
+def heater_cmd():
     selection = "You selected the option " + heater_state_var.get()
     print(selection)
 
 
-# create radio buttons and place them on the label frame
-on_radio = ttk.Radiobutton(lf, text='On', value='On', variable=heater_state_var, command = sel)
+on_radio = ttk.Radiobutton(hc_lf, text='On', value='On', variable=heater_state_var, command=heater_cmd)
 on_radio.grid(column=0, row=0, ipadx=10, ipady=10)
 
-off_radio = ttk.Radiobutton(lf, text='Off', value='Off', variable=heater_state_var, command = sel)
+off_radio = ttk.Radiobutton(hc_lf, text='Off', value='Off', variable=heater_state_var, command=heater_cmd)
 off_radio.grid(column=1, row=0, ipadx=10, ipady=10)
 
+# Temperature sensor control
+
+ts_lf = ttk.LabelFrame(root, text='Temperature sensor [X]')
+
+ts_lf.grid(column=0, row=1, padx=20, pady=20, sticky=tk.W)
+
+temp = tk.Text(ts_lf, height=1, width=10)
+temp['state'] = 'disabled'
+temp.grid(column=0, row=0, padx=20, pady=20)
+
+
+def refresh_temp():
+    print("Refresh")
+
+
+refresh_button = ttk.Button(
+    ts_lf,
+    text='Refresh',
+    command=refresh_temp
+)
+
+refresh_button.grid(column=1, row=0, padx=20, pady=20)
+
+#label = ttk.Label(ts_lf, text='This is a label')
+#label.grid(column=1, row=0, ipadx=10, ipady=10)
 
 root.mainloop()
