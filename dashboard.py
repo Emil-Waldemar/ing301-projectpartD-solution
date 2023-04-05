b6f4bc2ea6c2ee43c7638e12d8608f7191dbd557
Lars Michael Kristensen <lmkr@users.noreply.github.com>
2023-04-05 14:57:34 +0200

    dashboard code cleanup

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+# https://www.pythontutorial.net/tkinter/
 
 import logging
 
@@ -7,8 +8,6 @@ from dashboard_temperaturesensor import init_temperaturesensor
 
 import common
 
-# root/main window
-
 log_format = "%(asctime)s: %(message)s"
 logging.basicConfig(format=log_format, level=logging.INFO, datefmt="%H:%M:%S")
 
@@ -16,10 +15,7 @@ root = tk.Tk()
 root.geometry('300x300')
 root.title('ING301 SmartHouse Dashboard')
 
-
 init_lightbulb(root, common.LIGHTBULB_DID)
 init_temperaturesensor(root, common.TEMPERATURE_SENSOR_DID)
 
 root.mainloop()
-
-# https://www.pythontutorial.net/tkinter/
